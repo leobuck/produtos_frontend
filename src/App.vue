24fb6d4e6ba5f372652b0d4f1a3abf2c670cfc81
leonardobuck <leonardo@timais.com>
2019-12-20 15:04:12 -0300

    API REST Produtos frontend Vue JS

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
     </nav>
 
     <div class="container">
+      <ul>
+        <li v-for="(erro, index) of errors" :key="index">
+          campo
+          <b>{{ erro.field }}</b>
+          - {{ erro.defaultMessage }}
+        </li>
+      </ul>
+
       <form @submit.prevent="salvar">
         <label>Nome</label>
         <input type="text" placeholder="Nome" v-model="produto.nome" />
@@ -37,10 +45,10 @@
             <td>{{ produto.quantidade }}</td>
             <td>{{ produto.valor }}</td>
             <td>
-              <button class="waves-effect btn-small blue darken-1">
+              <button @click="editar(produto)" class="waves-effect btn-small blue darken-1">
                 <i class="material-icons">create</i>
               </button>
-              <button class="waves-effect btn-small red darken-1">
+              <button @click="remover(produto)" class="waves-effect btn-small red darken-1">
                 <i class="material-icons">delete_sweep</i>
               </button>
             </td>
@@ -57,31 +65,70 @@ export default {
   data() {
     return {
       produto: {
+        id: "",
         nome: "",
         quantidade: "",
         valor: ""
       },
-      produtos: []
+      produtos: [],
+      errors: []
     };
   },
   mounted() {
     this.listar();
   },
-  methods:{
+  methods: {
     listar() {
       Produto.listar().then(resposta => {
-        //console.log(resposta.data)
+        console.log(resposta.data); // eslint-disable-line no-console
         this.produtos = resposta.data;
-      })
+      });
     },
-    salvar(){
+    salvar() {
       //alert(this.produto.nome);
-      Produto.salvar(this.produto).then(resposta => {
-        this.produto = {};
-        alert(resposta.data);
-        alert('Salvo com sucesso!');
-        this.listar();
-      })
+
+      if (!this.produto.id) {
+        Produto.salvar(this.produto)
+          .then(resposta => {
+            this.produto = {};
+            console.log(resposta.data); // eslint-disable-line no-console
+            alert("Salvo com sucesso!");
+            this.listar();
+            this.errors = {};
+          })
+          .catch(e => {
+            console.log(e.response.data.errors); // eslint-disable-line no-console
+            this.errors = e.response.data.errors;
+          });
+      } else {
+        Produto.atualizar(this.produto)
+          .then(resposta => {
+            this.produto = {};
+            console.log(resposta.data); // eslint-disable-line no-console
+            alert("Atualizado com sucesso!");
+            this.listar();
+            this.errors = {};
+          })
+          .catch(e => {
+            console.log(e.response.data.errors); // eslint-disable-line no-console
+            this.errors = e.response.data.errors;
+          });
+      }
+    },
+    editar(produto) {
+      this.produto = produto;
+    },
+    remover(produto) {
+      if(confirm('Deseja excluir o produto?')) {
+
+        Produto.apagar(produto).then(resposta => {
+          console.log(resposta.data); // eslint-disable-line no-console
+          this.listar();
+          this.errors = {}
+        }).catch(e => {
+          this.errors = e.response.data.errors
+        })
+      }
     }
   }
 };
--- src/services/produtos.js
+++ src/services/produtos.js
@@ -8,5 +8,13 @@ export default {
 
     salvar: (produto) => {
         return http.post('produto', produto)
+    },
+
+    atualizar: (produto) => {
+        return http.put('produto', produto)
+    },
+
+    apagar: (produto) => {
+        return http.delete('produto', {data: produto})
     }
 }
